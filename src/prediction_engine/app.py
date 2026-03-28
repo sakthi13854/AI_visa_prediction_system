ee11880014eb597458fbc7bb7d93f3dc7df2d126
SAKTHI.S <sakthi138540@gmail.com>
2026-03-28 21:02:39 +0530

    Change app.run to use environment port

--- src/prediction_engine/app.py
+++ src/prediction_engine/app.py
@@ -13,4 +13,5 @@ app.register_blueprint(predict_bp,url_prefix='/predict')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
